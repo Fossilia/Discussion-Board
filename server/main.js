5bd9efb7345b3eb3d207e4d0c5060b5d4d9346c5
Faisal Bagalagel <uultradude@gmail.com>
2022-01-20 14:42:26 -0500

    Added test user

--- server/main.js
+++ server/main.js
@@ -1,9 +1,18 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { PostsCollection } from '/imports/api/PostsCollection';
 
 const insertPost = postText => PostsCollection.insert({ poster: poster, postText: postText, likes: likes, dislikes: dislikes });
 
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
 Meteor.startup(() => {
   // If the Links collection is empty, add some data.
-  
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
 });
