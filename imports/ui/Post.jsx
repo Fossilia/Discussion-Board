3c67d6473ee86e37d683183a5b4f9d3bdb47bb14
Faisal Bagalagel <uultradude@gmail.com>
2022-01-23 01:24:03 -0500

    added date value to posts

--- imports/ui/Post.jsx
+++ imports/ui/Post.jsx
@@ -3,13 +3,20 @@ import {Form, Button, Container, Card} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default Post = ({ post }) => {
+date = post.createdAt
+const [month, day, year]       = [date.getMonth(), date.getDate(), date.getFullYear()]
+const [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()]
+const months = ["Jan","Feb","Mar","April","May","June","July","Aug","Sep","Oct","Nov","Dec"];
   return (
     <>
       <Card>
           <Card.Body>
-            <Card.Title>{post.poster}</Card.Title>
-            <Card.Text>
+            <Card.Text><u>{post.poster}</u></Card.Text>
+            <Card.Title>
               {post.postText}
+            </Card.Title>
+            <Card.Text style={{fontSize: 12}}>
+              {months[month]} {day}, {date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}
             </Card.Text>
         </Card.Body>
       </Card>
